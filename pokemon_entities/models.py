2aed9c76d1631bed71a738df05617ad1e9823712
aosothra <arhdam@yandex.ru>
2022-04-12 15:33:58 +0400

    add title locale fields for Pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    title_en = models.CharField(max_length=200, default='')
+    title_jp = models.CharField(max_length=200, default='')
     image = models.ImageField(upload_to='pokeimgsp', null=True)
     description = models.TextField(default='Этот покемон еще не описан')
 
