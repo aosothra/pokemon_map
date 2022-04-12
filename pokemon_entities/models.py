b5d3ee1be82878c97b4623fc9071f70fcb987eea
aosothra <arhdam@yandex.ru>
2022-04-12 18:59:46 +0400

    add backward relation to PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,8 +6,8 @@ class Pokemon(models.Model):
     '''Вид покемона'''
 
     title = models.CharField('Имя (рус.)', max_length=200)
-    title_en = models.CharField('Имя (анг.)', max_length=200, default='', blank=True)
-    title_jp = models.CharField('Имя (яп.)', max_length=200, default='', blank=True)
+    title_en = models.CharField('Имя (анг.)', max_length=200, blank=True)
+    title_jp = models.CharField('Имя (яп.)', max_length=200, blank=True)
     image = models.ImageField('Изображение', upload_to='pokeimgsp', null=True)
     description = models.TextField('Описание', default='Этот покемон еще не описан', blank=True)
     previous_evolution = models.ForeignKey(
@@ -26,7 +26,11 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     '''Особь-покемон'''
 
-    pokemon = models.ForeignKey(Pokemon, verbose_name='Вид покемона', on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(
+        Pokemon, 
+        verbose_name='Вид покемона', 
+        on_delete=models.CASCADE,
+        related_name='entities')
     lat = models.FloatField('Координата по широте')
     lon = models.FloatField('Координата по долготе')
     appearance_at = models.DateTimeField('Дата появления', default=None, null=True, blank=True)
